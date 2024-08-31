2cd7f0b0244f4ad4022f97ef0b75471a6051c553
eyadjs <eyadjs03@gmail.com>
2024-08-31 17:29:40 -0400

    ui

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,43 @@ function App() {
 
   return (
     <div>
-      <h1>hi</h1>
+      <div className='navbar'>navbar</div>
+      <div className='title-section'>
+        <div className='left'>
+          <div className='title'>
+            <p>CoLabel</p>
+          </div>
+          <div className='subtitle'>
+            <p>Collaborate on labelling training datasets</p>
+          </div>
+          <div className='description'>
+            <p>Expedite the boring, agonizing part of developing AI models</p>
+          </div>
+          <div className='get-started'>
+            <button>Get Started</button>
+          </div>
+        </div>
+        <div className='right'>
+          <div className='square-1'></div>
+          <div className='square-2'></div>
+          <div className='square-3'></div>
+          <div className='square-4'></div>
+        </div>
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Link to={'/dashboard'}><h1>Dashboard</h1></Link>} />
