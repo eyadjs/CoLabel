63e6d603c840f12b7e9c4bd295ab8cc825d7414c
Eyad Saeed <eyadjs03@gmail.com>
2024-08-31 21:06:41 -0400

    fixed homepage

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Dashboard from './components/dashboard';
 import FilePage from './components/filePage';
 import LabelSetup from './components/labelSetup';
 import Labelling from './components/labelling';
+import Home from './components/home';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 export const getRawFileName = (fileName) => {
@@ -18,33 +19,12 @@ export const getRawFileName = (fileName) => {
 function App() {
 
   return (
+
     <div>
       <div className='navbar'>
           <img className='logo-black' src="logo-black.png"/>
       </div>
       
-      <div className='title-section'>
-        <div className='left'>
-          <div className='title'>
-            <p>CoLabel</p>
-          </div>
-          <div className='subtitle'>
-            <p>Collaborate on labelling training datasets</p>
-          </div>
-          <div className='description'>
-            <p>Expedite the boring, agonizing part of developing AI models</p>
-          </div>
-          <div className='get-started'>
-            <button>Get Started</button>
-          </div>
-        </div>
-        <div className='right'>
-          <div className='square-1'></div>
-          <div className='square-2'></div>
-          <div className='square-3'></div>
-          <div className='square-4'></div>
-        </div>
-      </div>
 
 
 
@@ -61,7 +41,7 @@ function App() {
 
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Link to={'/dashboard'}><h1>Dashboard</h1></Link>} />
+          <Route path='/' element={<Home />} />
           <Route path='/dashboard' element={<Dashboard />} />
           <Route path='/dashboard/filePage/:fileName' element={<FilePage />} />
           <Route path='/labelSetup/:fileName' element={<LabelSetup />}></Route>
---
+++ client/src/components/home.js
@@ -0,0 +1,32 @@
+const Home = () => {
+
+    return (
+        <div>
+                  <div className='title-section'>
+        <div className='left'>
+          <div className='title'>
+            <p>CoLabel</p>
+          </div>
+          <div className='subtitle'>
+            <p>Collaborate on labelling training datasets</p>
+          </div>
+          <div className='description'>
+            <p>Expedite the boring, agonizing part of developing AI models</p>
+          </div>
+          <div className='get-started'>
+            <button>Get Started</button>
+          </div>
+        </div>
+        <div className='right'>
+          <div className='square-1'></div>
+          <div className='square-2'></div>
+          <div className='square-3'></div>
+          <div className='square-4'></div>
+        </div>
+      </div>
+        </div>
+    )
+}
+
+
+export default Home;
