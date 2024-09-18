75028e2d146338b12a9fef89c1485ed224d7c971
eyadjs <eyadjs03@gmail.com>
2024-09-17 20:29:20 -0400

    tutorial updates

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -31,7 +31,7 @@ const Home = () => {
             <p>Collaborate on labelling training datasets</p>
           </div>
           <div className='description'>
-            <p>Expedite the boring, agonizing part of developing AI models</p>
+            <p>Expedite the <span className='highlight'>boring</span>, <span className='highlight'>agonizing</span> part of developing AI models</p>
           </div>
           <div className='get-started'>
             <Link to={"/dashboard"}><button>Get Started</button></Link>
@@ -46,7 +46,7 @@ const Home = () => {
 
       <div className='about-section' id='about'>
         <div className='heading'>
-          <p>Why CoLabel?</p>
+          <p>Why <span className='highlight'>CoLabel</span>?</p>
         </div>
         <div className='about-blocks'>
           <div className='row row-1'>
@@ -86,9 +86,9 @@ const Home = () => {
         <p className='heading'>Tutorial</p>
         <div className='tutorial-block'>
           <p className='tutorial-text'>After setting up your account, proceed to your dashboard where you will see your currently uploaded files.</p>
-          <p className='tutorial-text'>Here, you can add, delete, access, and manage your projects.</p>
+          <p className='tutorial-text'>Here, you can add, delete, access, manage, and check the progress of your projects:</p>
           <div className='dashboard'>
-          <p style={{left:"20%", bottomMargin:"5%", fontSize:"30px", fontWeight:"200"}}>Manage your projects</p>
+          {/* <p style={{left:"20%", bottomMargin:"5%", fontSize:"30px", fontWeight:"200"}}>Manage your projects</p> */}
           <div className='file-table'>
           <div className='file-rows'>
             <div className='column'>
@@ -135,7 +135,16 @@ const Home = () => {
           <p className='warning'>Ensure you have uploaded a .csv file as your unlabelled dataset!</p>
           <p className='warning'>Ensure your file is comma-seperated, NOT semicolon seperated!</p>
           <p className='warning'>Ensure your file does NOT yet contain the field you would like to add labelled data for, CoLabel will handle this for you!</p>
-          <p className='tutorial-text'>Once you have opened a project for the first time, you will be prompted to set certain parameters.</p>
+          <p className='tutorial-text'>Once you have opened a project for the first time, you will be prompted to set certain parameters:</p>
+          <ul>
+            <li><span className='blt-hdg'>Labels</span>: All possible labels you would like to give your new field. For example, if the new field was "gender", possible labels would be <span className='focus'>male</span> and <span className='focus'>female</span>.</li>
+            <li><span className='blt-hdg'>Label Field Name</span>: The name of the new column you would like to add. According to the previous example, this would be <span className='focus'>gender</span>. </li>
+            <li><span className='blt-hdg'>Fields To Label Upon</span>: For bigger datasets, looking at an entire row when deciding how to label a field can be redundant. This option allows you to pick which columns from your dataset you would like to see when deciding what label to choose. For our previous example, this could be <span className='focus'>name</span> as that is usually sufficient to decide gender.</li>
+          </ul>
+          <p className='tutorial-text'>After setting up your project, you will be asked to determine Chunk Size before each labelling session:</p>
+          <ul>
+            <li><span className='blt-hdg'>Chunk Size</span> determines the number of rows from your dataset you would like to label in this particular session. If your dataset contains 800 entries but you only have time to label 50 entries, you may set your chunk size to 50, and only 50 entries will be shown for labelling on that particular session.</li>
+          </ul>
         </div>
       </div>
         </div>
