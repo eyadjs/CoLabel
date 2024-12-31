75d311f78e1f65d99f97ac4b37c322af783b0537
eyadjs <eyadjs03@gmail.com>
2024-12-30 19:22:28 -0500

    firebase keys in .env for vercel

--- server/upload.js
+++ server/upload.js
@@ -1,13 +1,16 @@
 const multer = require('multer');
 const admin = require('firebase-admin');
-const serviceAccountBase64 = process.env.SERVICE_ACCOUNT_KEY;
-const serviceAccount = JSON.parse(Buffer.from(serviceAccountBase64, 'base64').toString('utf8'));
+const serviceAccount = require('./serviceAccountKey.json'); // Update with the path to your Firebase service account key
 require('dotenv').config()
 
 const storageBucket = process.env.FIREBASE_URL
 // Initialize Firebase Admin SDK
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    "projectId": process.env.FIREBASE_PROJECT_ID,
+    "private_key": process.env.FIREBASE_PRIVATE_KEY,
+    "client_email": process.env.FIREBASE_CLIENT_EMAIL
+  }),
   storageBucket: storageBucket, // Replace with your Firebase Storage bucket
 });
 
