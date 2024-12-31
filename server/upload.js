0ab360bc430ddc0f7eb28c1006c088a3e6fb74d6
eyadjs <eyadjs03@gmail.com>
2024-12-30 19:54:53 -0500

    fix initializeApp() twice

--- server/upload.js
+++ server/upload.js
@@ -8,12 +8,12 @@ const storageBucket = process.env.FIREBASE_URL
 // Initialize Firebase Admin SDK
 admin.initializeApp({
   credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    "projectId": process.env.FIREBASE_PROJECT_ID,
+    "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    "client_email": process.env.FIREBASE_CLIENT_EMAIL
   }),
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET, // Add your storage bucket here
-}, );
+  storageBucket: storageBucket, // Replace with your Firebase Storage bucket
+});
 
 const bucket = admin.storage().bucket();
 
